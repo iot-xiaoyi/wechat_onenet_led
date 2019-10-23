d67cc8e81480baa5e2a3db5bcc201c426e3fc6e7
brown <16601747610@163.com>
2019-10-23 23:27:53 +0800

    修改LED页面显示逻辑，开关不能同时打开，  增加离开页面停止获取数据功能

--- pages/led/led.js
+++ pages/led/led.js
@@ -1,6 +1,6 @@
 var WxAutoImage = require('../../js/wxAutoImageCal.js');
 var onenet = require('../../js/onenet.js');
-
+var API_KEY = "eCQJZEKoyVqA5qV4ef3qTH2OZzo="
 var app = getApp();
 
 Page({
@@ -13,9 +13,13 @@ Page({
     ],
     room_name:"",
     input_name:"",
-    id:0
+    id:0,
+    redSwitchChecked:false,
+    greenSwitchChecked:false,
+    blueSwitchChecked:false,
+    switchFlag:false,
+    myintervalid:0,
   },
-
   onLoad: function (e) {
     var that = this;
     console.log("onloading......");
@@ -23,14 +27,9 @@ Page({
     that.setData({
       id:e.id
     })
-    setInterval(function () {
-      var dat = onenet.getDeviceStatus(e.id);
-      if (dat)
-        console.log("connect to cloud success!");
-      else
-        console.log("connect to cloud error!");
-
-        onenet.getDataPoints(e.id)
+    that.getDataPoints(e.id)
+    that.data.myintervalid = setInterval(function () {
+      that.onShow()
     }, 3000)
 
     //get storage data
@@ -47,6 +46,25 @@ Page({
       console.log("get stroage data error!")
     }
   },
+  onShow: function (e) {
+    var that = this
+    // var dat = onenet.getDeviceStatus(e.id);
+    // if (dat)
+    //   console.log("connect to cloud success!");
+    // else
+    //   console.log("connect to cloud error!");
+
+    that.getDataPoints(that.data.id)
+  },
+  onHide: function () {
+    // 页面隐藏
+    clearInterval(this.data.myintervalid);
+  },
+
+  onUnload: function () {
+    // 页面关闭
+    clearInterval(this.data.myintervalid);
+  },
 
   btn_connect_fun: function (e) {
     console.log("ready to connect to onenet!");
@@ -60,36 +78,72 @@ Page({
   },
   btn_red_led_fun: function (e) {
     var that = this;
-    if (true == e.detail.value)
+    if (false == e.detail.value)
     {
       console.log("ready to open red led!");
       onenet.sendCmd(that.data.id, "0")
+      that.setData({
+        redSwitchChecked: false,
+        greenSwitchChecked: false,
+        blueSwitchChecked: false
+      })
     }else
     {
       console.log("ready to close red led!");
       onenet.sendCmd(that.data.id, "2")
-    }
-  },
-  btn_blue_led_fun: function (e) {
-    var that = this;
-    if (true == e.detail.value) {
-      console.log("ready to open blue led!");
-      onenet.sendCmd(that.data.id, "0")
-    } else {
-      console.log("ready to close blue led!");
-      onenet.sendCmd(that.data.id, "4")
+      that.setData({
+        redSwitchChecked: true,
+        greenSwitchChecked: false,
+        blueSwitchChecked: false,
+        switchFlag:true
+      })
     }
   },
   btn_green_led_fun: function (e) {
     var that = this;
-    if (true == e.detail.value) {
+    if (false == e.detail.value) {
       console.log("ready to open blue led!");
       onenet.sendCmd(that.data.id, "0")
+      that.setData({
+        redSwitchChecked: false,
+        greenSwitchChecked: false,
+        blueSwitchChecked: false
+      })
     } else {
       console.log("ready to close blue led!");
       onenet.sendCmd(that.data.id, "3")
+      that.setData({
+        redSwitchChecked: false,
+        greenSwitchChecked: true,
+        blueSwitchChecked: false,
+        switchFlag: true
+      })
     }
   },
+  btn_blue_led_fun: function (e) {
+    var that = this;
+    if (false == e.detail.value) {
+      console.log("ready to open blue led!");
+      onenet.sendCmd(that.data.id, "0")
+      that.setData({
+        redSwitchChecked: false,
+        greenSwitchChecked: false,
+        blueSwitchChecked: false
+      })
+    } else {
+      console.log("ready to close blue led!");
+      onenet.sendCmd(that.data.id, "4")
+      that.setData({
+        redSwitchChecked: false,
+        greenSwitchChecked: false,
+        blueSwitchChecked: true,
+        switchFlag: true
+      })
+    }
+  },
+//显示遮罩
+
+//设置房间
   set_room: function () {
     this.setData({
       showModal: true
@@ -141,11 +195,13 @@ Page({
   },
 
   //onenet interfce
-  getDataPoints:function(id) {
+  getDataPoints: function (id) {
+    var that = this
     var deviceConnected
-  //查看设备连接状态，并刷新按钮状态
-  wx.request({
-      url: "http://api.heclouds.com/devices/" + id + "/datapoints?datastream_id=color&start=2019-09-01T00:00:00&limit=100",
+    var color_value = 0
+    //查看设备连接状态，并刷新按钮状态
+    wx.request({
+      url: "http://api.heclouds.com/devices/" + id + "/datapoints?datastream_id=color",
       header: {
         'content-type': 'application/x-www-form-urlencoded',
         "api-key": API_KEY
@@ -155,6 +211,7 @@ Page({
       },
       success(res) {
         console.log(res)
+        deviceConnected = true
         // if (res.data.data.online) {
         //   console.log("设备已经连接")
         //   deviceConnected = true
@@ -162,20 +219,53 @@ Page({
         //   console.log("设备还未连接")
         //   deviceConnected = false
         // }
+        if (that.data.switchFlag != true)
+        {
+          color_value = res.data.data.datastreams[0].datapoints[0].value
+          switch (parseInt(color_value)) {
+            case 1:
+              that.setData({
+                redSwitchChecked: false,
+                greenSwitchChecked: false,
+                blueSwitchChecked: false
+              })
+              break;
+            case 2:
+              that.setData({
+                redSwitchChecked: true,
+                greenSwitchChecked: false,
+                blueSwitchChecked: false
+              })
+              break;
+            case 3:
+              that.setData({
+                redSwitchChecked: false,
+                greenSwitchChecked: true,
+                blueSwitchChecked: false
+              })
+              break;
+            case 4:
+              that.setData({
+                redSwitchChecked: false,
+                greenSwitchChecked: false,
+                blueSwitchChecked: true
+              })
+              console.log("color_value is ", color_value)
+              console.log("blueSwitchChecked is ", that.data.blueSwitchChecked)
+              break;
+          }
+          console.log("color_value is ", color_value)
+        }else{
+          that.setData({
+            switchFlag:false
+          })
+        }
       },
       fail(res) {
         console.log("请求失败")
         deviceConnected = false
-      },
-      complete() {
-        if (deviceConnected) {
-          console.log("complete ok")
-          return true
-        } else {
-          console.log("complete error")
-          return false
-        }
       }
     })
   },
+
 })
